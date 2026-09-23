7af38009a8a42053d8e0ffd6543c85e6a9b7779e
Lautaro Squicciarini <squicciarinilautarosetter@gmail.com>
2026-09-23 16:39:13 -0300

    solucion al error 404 y boton del login

--- stock-montech/src/supabase.js
+++ stock-montech/src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Supabase
-const supabaseUrl = 'https://ievyjsjyrsupcldyryti.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlldnlqc2p5cnN1cGNsZHlyeXRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAxMDI3NjIsImV4cCI6MjEwNTY3ODc2Mn0.fKHJyLZ1WCSUvBUUTrJi-M5RxgZm6DuFT2V9EpQWMoU'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
